fc17db1971cdf6fcc850780b5815a42fab835785
ksiminski <ksiminski@polsl.pl>
2024-12-09 13:42:30 +0100

    szkielet maina

--- source/main.cpp
+++ source/main.cpp
@@ -2,7 +2,22 @@
 
 
 
-int main ()
+int main (int ile, char * params[])
 {
+    // czy parametry sa poprawne?
+    // auto [nazwa_pliku_wejsciowego, nazwa_pliku_wyjsciowego] = pobierz_wartosc_parametrow(ile, params);
+
+    // if (not nazwa_pliku_wejsciowego.empty() and not nazwa_pliku_wyjsciowego.empty())
+    // {
+        // działamy!
+        // graf konflikty = wczytaj_konflikty(nazwa_pliku_wejsciowego);
+        // pokoloruj_wierzcholki_grafu(konflikty);
+        // zapisz_kolory (nazwa_pliku_wyjsciowego);
+    // }
+    // else
+    // {
+        // help();
+    // }
+
     return 0;
 }
