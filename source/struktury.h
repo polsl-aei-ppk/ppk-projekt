2e06df7ac2f0c8d2d81226f17e36682ea76cf534
ksiminski <ksiminski@polsl.pl>
2024-12-09 14:12:22 +0100

    strukutra danych dla grafu

--- source/struktury.h
+++ source/struktury.h
@@ -1,5 +1,19 @@
 #ifndef STRUKTURY_H
 #define STRUKTURY_H
 
+#include <map>
+#include <string>
+#include <vector>
+
+struct graf
+{
+    struct wierzcholki
+    {
+        unsigned int kolor;
+        std::vector<std::string> sasiedzi;
+    };
+
+    std::map<std::string,wierzcholki> wezly;
+};
 
 #endif
